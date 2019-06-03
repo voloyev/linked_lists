d9b36f429e104a2cb919d524e3db4fb8faf612ac
Volodymyr Yevtushenko <voloyev@vivaldi.net>
2019-06-03 23:12:49 +0300

    Add #push

--- src/first.rs
+++ src/first.rs
@@ -23,4 +23,14 @@ impl List {
 
         self.head = Link::More(new_node);
     }
+
+    pub fn pop(&mut self) -> Option<i32> {
+        match mem::replace(&mut self.head, Link::Empty) {
+            Link::Empty => None,
+            Link::More(node) => {
+                self.head = node.next;
+                Some(node.elem)
+            }
+        }
+    }
 }
