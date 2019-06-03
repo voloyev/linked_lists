a6b5319a29235b798749f94ad9ce35332e4d6792
Volodymyr Yevtushenko <voloyev@vivaldi.net>
2019-06-04 00:31:42 +0300

    Impl iterator

--- src/second.rs
+++ src/second.rs
@@ -49,6 +49,20 @@ impl<T> Drop for List<T> {
     }
 }
 
+pub struct IntoIter<T>(List<T>);
+
+impl<T> List<T> {
+    pub fn into_iter(self) -> IntoIter<T> {
+        IntoIter(self)
+    }
+}
+
+impl<T> Iterator for IntoIter<T> {
+    type Item = T;
+    fn next(&mut self) -> Option<Self::Item> {
+        self.0.pop()
+    }
+}
 #[cfg(test)]
 mod test {
     use super::List;
@@ -113,4 +127,17 @@ mod test {
         assert_eq!(list.pop(), Some(42));
     }
 
+    #[test]
+    fn into_iter() {
+        let mut list = List::new();
+        for el in &[1, 2, 3] {
+            list.push(*el);
+        }
+
+        let mut iter = list.into_iter();
+        assert_eq!(iter.next(), Some(3));
+        assert_eq!(iter.next(), Some(2));
+        assert_eq!(iter.next(), Some(1));
+        assert_eq!(iter.next(), None);
+    }
 }
