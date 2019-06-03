da73c9fdf8ed0ee207bf86826023e28d9318310a
Volodymyr Yevtushenko <voloyev@vivaldi.net>
2019-06-04 00:22:19 +0300

    Add tests for peek_* methods

--- src/second.rs
+++ src/second.rs
@@ -92,12 +92,25 @@ mod test {
     fn peek() {
         let mut list = List::new();
         assert_eq!(list.peek(), None);
-        assert_eq!(list.peek_mut(), None);
-        list.push(1);
-        list.push(2);
-        list.push(3);
+        for el in &[1, 2, 3] {
+            list.push(*el);
+        }
 
         assert_eq!(list.peek(), Some(&3));
-        assert_eq!(list.peek_mut(), Some(&mut 3));
     }
+
+    #[test]
+    fn peek_mut() {
+        let mut list = List::new();
+        assert_eq!(list.peek_mut(), None);
+
+        for el in &[1, 2, 3] {
+            list.push(*el);
+        }
+        assert_eq!(list.peek_mut(), Some(&mut 3));
+        list.peek_mut().map(|value| *value = 42);
+        assert_eq!(list.peek(), Some(&42));
+        assert_eq!(list.pop(), Some(42));
+    }
+
 }
