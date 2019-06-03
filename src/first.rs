1ea2e2180179f8abd5795eb4503407dc73e78e33
Volodymyr Yevtushenko <voloyev@vivaldi.net>
2019-06-03 23:04:12 +0300

    Add push method

--- src/first.rs
+++ src/first.rs
@@ -1,3 +1,5 @@
+use std::mem;
+
 pub struct Node {
     elem: i32,
     next: Link,
@@ -10,4 +12,15 @@ pub struct List {
 pub enum Link {
     Empty,
     More(Box<Node>),
+}
+
+impl List {
+    pub fn push(&mut self, elem: i32) {
+        let new_node = Box::new(Node {
+            elem: elem,
+            next: mem::replace(&mut self.head, Link::Empty),
+        });
+
+        self.head = Link::More(new_node);
+    }
 }
