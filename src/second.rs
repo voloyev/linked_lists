24be32d9ec0157374b5b1d30bf93c494286d972c
Volodymyr Yevtushenko <voloyev@vivaldi.net>
2019-06-03 23:36:39 +0300

    Copy first mod content into second mod

--- src/second.rs
+++ src/second.rs
@@ -1,1 +1,88 @@
+use std::mem;
 
+pub struct Node {
+    elem: i32,
+    next: Link,
+}
+
+pub struct List {
+    head: Link,
+}
+
+pub enum Link {
+    Empty,
+    More(Box<Node>),
+}
+
+impl List {
+    pub fn new() -> Self {
+        List { head: Link::Empty }
+    }
+    pub fn push(&mut self, elem: i32) {
+        let new_node = Box::new(Node {
+            elem: elem,
+            next: mem::replace(&mut self.head, Link::Empty),
+        });
+
+        self.head = Link::More(new_node);
+    }
+
+    pub fn pop(&mut self) -> Option<i32> {
+        match mem::replace(&mut self.head, Link::Empty) {
+            Link::Empty => None,
+            Link::More(node) => {
+                self.head = node.next;
+                Some(node.elem)
+            }
+        }
+    }
+}
+
+impl Drop for List {
+    fn drop(&mut self) {
+        let mut cur_link = mem::replace(&mut self.head, Link::Empty);
+
+        while let Link::More(mut boxed_node) = cur_link {
+            cur_link = mem::replace(&mut boxed_node.next, Link::Empty);
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::List;
+
+    #[test]
+    fn test_new() {
+        let mut list = List::new();
+        assert_eq!(list.pop(), None);
+    }
+
+    #[test]
+    fn test_push() {
+        let mut list = List::new();
+
+        assert_eq!(list.pop(), None);
+        for i in &[1, 2, 3] {
+            list.push(*i);
+        }
+
+        assert_eq!(list.pop(), Some(3));
+        assert_eq!(list.pop(), Some(2));
+        assert_eq!(list.pop(), Some(1));
+        assert_eq!(list.pop(), None);
+    }
+
+    #[test]
+    fn test_pop() {
+        let mut list = List::new();
+        for i in &[1, 2, 3] {
+            list.push(*i);
+        }
+
+        assert_eq!(list.pop(), Some(3));
+        assert_eq!(list.pop(), Some(2));
+        assert_eq!(list.pop(), Some(1));
+        assert_eq!(list.pop(), None);
+    }
+}
