3bc2aedd84c2d61dfc5a0ff8cb11006dfb6bd0bc
Volodymyr Yevtushenko <voloyev@vivaldi.net>
2026-01-29 22:53:44 +0200

    Add initial version of peek

--- src/second.rs
+++ src/second.rs
@@ -29,6 +29,14 @@ impl<T> List<T> {
             node.elem
         })
     }
+
+    pub fn peek(&self) -> Option<&T> {
+        self.head.as_ref().map(|node| &node.elem)
+    }
+
+    pub fn peek_mut(&mut self) -> Option<&mut T> {
+        self.head.as_mut().map(|node| &mut node.elem)
+    }
 }
 
 impl<T> Drop for List<T> {
